6bd98c02f9e0b18bc7c6a1dfd5971554862882ff
5yedG1br4n <119901061+5yedG1br4n@users.noreply.github.com>
2024-03-03 21:19:10 +0530

    hope this logo is fixed now

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import TrollFace from "./assets/Troll_Face.svg";
 
 export default function Header() {
   return (
     <header className="flex bg-gradient-to-r from-[#672280] to-[#A626D3] h-16 items-center px-6 tracking-tight text-[#ffffff]">
       <div className="flex gap-2 items-center">
         <img
-          src="/src/assets/Troll_Face.svg"
+          src={TrollFace}
           alt="A troll face which serves as a logo for the app"
           className="h-8"
         />
